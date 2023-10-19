dd8faba6b13c91cef67cfa4c5f760cbbc5f84b3b
Jon Ortiz <jonnatano94@gmail.com>
2023-10-19 15:31:18 -0700

    Add togglerOpen state var to NavBar to handle responsive dropdown to work and add margin top to navBar container

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NavBar.css';
 import BimmersBanner from './../../assets/images/banner-bimmers.jpg'
 
 const NavBar = () => {
+  const [togglerOpen, setTogglerOpen] = useState(false);
+
+  const togglerOnClick = (e) => {
+    e.preventDefault();
+    setTogglerOpen(current => !current)
+  }
+
     return (
       <div className='nav-bar-container'>
         <img src={BimmersBanner} alt="not-found"/>
@@ -10,14 +17,14 @@ const NavBar = () => {
             <div className="container">
               <nav className="site-navbar">
                 {/* <a href="#home" className="site-logo">logo</a> */}
-                <ul>
+                <ul className={`${togglerOpen ? 'open' : ''}`}>
                   <li><a href="/">home</a></li>
                   <li><a href="/services">service</a></li>
                   <li><a href="/specials">specials</a></li>
                   <li><a href="/contact">contact</a></li>
                 </ul>
         
-                <button className="nav-toggler">
+                <button className={`nav-toggler${togglerOpen ? ' toggler-open' : ''}`} onClick={togglerOnClick}>
                   <span></span>
                 </button>
               </nav>
