d89c8f5be8e3de8bc06ae3b00cf244cec6822441
Jon Ortiz <jonnatano94@gmail.com>
2023-10-19 14:34:50 -0700

    Add Router and Routes to App and index

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavBar from './components/navbar/NavBar';
-
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import './App.css'
 const App = () => {
     return (
       <div className='app-container'>
-        <NavBar/>
+          <Routes>
+
+            <Route path="/" element={<NavBar/>}/>
+          </Routes>
       </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      {/* <Routes> */}
+        <App />
+      {/* </Routes> */}
+    </Router>
   </React.StrictMode>
 );
 
