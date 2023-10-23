771586497ac8e495923b2278b642e735a8a02bd9
Jon Ortiz <jonnatano94@gmail.com>
2023-10-23 15:31:19 -0700

    Initial component shell for Home with Carousel component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Carousel from '../carousel/Carousel'
+const Home = () => {
+
+    return (
+        <div>
+            HOME
+            <Carousel/>
+        </div>
+    )
+}
+
+export default Home
