4a2f2687f47fed407bfdfe6b6f6f60643f4be9d5
Jon Ortiz <jonnatano94@gmail.com>
2023-11-22 15:28:49 -0800

    remove commented img import in RightSide component

--- src/components/Info/RightSideInfo.jsx
+++ src/components/Info/RightSideInfo.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import LocationMap from '../location/LocationMap'
-import ShuttleImg from './../../assets/images/shuttle_img.png'
 import './RightSideInfo.css'
 const RightSideInfo = () => {
 
