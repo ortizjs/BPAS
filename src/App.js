750ba76139d794215a1f7888a27e2bb985bc9189
Jon Ortiz <jonnatano94@gmail.com>
2023-11-22 15:21:18 -0800

    Installed packages and removed console.log in App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import {Routes, Route, useLocation} from 'react-router-dom';
 import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home';
 import Services from './components/services/Services';
+import Reviews from './components/reviews/Reviews';
 import './App.css'
 
 const App = () => {
     const location = useLocation();
     let path = location.pathname.split("/")[1];
-    console.log(path);
     return (
       <div className={`app-container${path === "services" ? ' extended-down' : ''}`}>
         <NavBar/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/services" element={<Services/>}/>
+          <Route path="/reviews" element={<Reviews/>}/>
         </Routes>
         <hr className="splash-page-main-div-hr" align="center"/>
       </div>
